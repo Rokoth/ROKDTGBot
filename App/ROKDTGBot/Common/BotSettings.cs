148a60d45c432804b13c396e2d693c7d4a03b4b2
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2026-06-20 11:13:20 +0500

    рефакторинг

--- App/ROKDTGBot/Common/BotSettings.cs
+++ App/ROKDTGBot/Common/BotSettings.cs
@@ -1,8 +1,13 @@
 ﻿namespace Common
 {
+    /// <summary>
+    /// Настройки подключения к боту
+    /// </summary>
     public class BotSettings
     {       
+        /// <summary>
+        /// Токен
+        /// </summary>
         public string Token { get; set; } = "";
-
     }
 }
